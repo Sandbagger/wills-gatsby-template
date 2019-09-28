7821801645a58445271f647bc18e8c6f8a359e99
Sandbagger <wneal_19@yahoo.co.uk>
2019-09-28 15:15:58 +0100

    Added more tests for navbar items

--- src/components/header.test.js
+++ src/components/header.test.js
@@ -3,14 +3,24 @@ import { render } from "@testing-library/react"
 import Header from "./header"
 
 
-test("Logo is 'William Neal", () => {
+test("Navbar has item1", () => {
     const { getByTestId } = render(<Header/>)
-
-    expect(getByTestId("logo")).toHaveTextContent("William Neal")
+    expect(getByTestId("nav-item-1")).toHaveTextContent("item1")
 })
 
-test("Header has 'Home for nav item", () => {
+test("Navbar has item2", () => {
+    const { getByTestId } = render(<Header/>)
+    expect(getByTestId("nav-item-2")).toHaveTextContent("item2")
+})
+
+test("Navbar has item3", () => {
+    const { getByTestId } = render(<Header/>)
+    expect(getByTestId("nav-item-3")).toHaveTextContent("item3")
+})
+
+test("Navbar has item4", () => {
     const { getByTestId } = render(<Header/>)
 
-    expect(getByTestId('nav-item-home')).toHaveTextContent("Home")
+    expect(getByTestId('nav-item-4')).toHaveTextContent("item4")
 })
+
