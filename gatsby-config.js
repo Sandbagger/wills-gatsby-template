60007e44830f290a2b6ebcd9d0327804fb7c31eb
Sandbagger <wneal_19@yahoo.co.uk>
2019-09-28 19:07:44 +0100

    Added JSON transformer config

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,13 @@ module.exports = {
           include: /assets/ 
         }
       }
+    },
+    "gatsby-transformer-json", {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'data',
+        path: `${__dirname}/src/data/`
+      }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
