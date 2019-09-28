b1af607e2bc6e11cb7f569ae475b083e4e9e5153
Sandbagger <wneal_19@yahoo.co.uk>
2019-09-28 18:26:28 +0100

    Added theme object.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,26 @@
-import { Link } from "gatsby";
 import React from "react";
-import { StyledHeroImage } from '../components/image';
+import { StyledHeroImage } from '../components/BackgroundImage';
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import  { ThemeProvider } from 'styled-components';
 
 require('react-dom');
+
+const theme = {
+  primary: '#0B3954', 
+  secondary: '#BFD7EA',
+  background: 'FEFFFE'
+}
 
 const IndexPage = () => {
   
   return (
+   <ThemeProvider theme={theme}>
   <Layout>
     <SEO title="Home" />
     <StyledHeroImage/>
   </Layout>
+  </ThemeProvider> 
 )
   }
 
