d3ded5a0ebd01061fffaa68502d3171450088c6f
Sandbagger <wneal_19@yahoo.co.uk>
2019-09-30 00:08:58 +0100

    Added an exmple of a grid displaying items. WIP,

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,78 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, {useState} from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import {Categories} from "../components/Categories";
+import {Items} from "../components/Items"
+import styled from 'styled-components';
+import SEO from "../components/seo";
+import  { ThemeProvider } from 'styled-components';
+import { theme } from '../theme'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+const Container = styled.div`
+display: flex;
+padding-top: 100px;
+flex-direction: column;
+`
+const Flex = styled.div`
+display: flex;
+justify-content: center;
+justify-items: center;
+align-items: center;
+`
+const Grid = styled.div`
+display: grid;
+grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+grid-gap: 1rem;
+align-items: center;
+`
 
-const SecondPage = () => (
+const SecondPage = ({ data }) => {
+ const items = data.allItemsJson.edges.map(edge => edge.node);
+const [filteredItems, setFilteredItems] = useState(items);
+ const catagories = new Set([...items.map(item => item.catagory)]);
+ const filterItems = catagory => items.filter(catagory => items.catagory === catagory)
+ console.log('items', items, catagories, filteredItems) 
+  return (
+    <ThemeProvider theme={theme}>
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+  
+    <Container>
+        <Flex>
+          <div>Suggested catagories:</div>
+          <Categories items={[...catagories]}/>
+        </Flex>
+    
+      <Grid>
+        <Items items={filteredItems}/>
+      </Grid>
+    </Container>
+ 
   </Layout>
+  </ThemeProvider>
 )
+  };
+
+  export const query = graphql`
+  query SecondPage {
+    allItemsJson {
+      edges {
+        node {
+          name
+          catagory
+          image {
+            publicURL
+            childImageSharp {
+              fluid(maxWidth: 300) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  `
+
 
 export default SecondPage
