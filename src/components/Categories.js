f877688a0c9854b72453eefbf06ca5f7714fe389
Sandbagger <wneal_19@yahoo.co.uk>
2019-09-30 00:03:06 +0100

    Flat structure to be used in a CSS Grid

---
+++ src/components/Categories.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import styled from 'styled-components';
+import { theme } from '../theme'
+
+
+
+const Pill = styled.button`
+  border: 2px solid ${theme.secondary};
+  color: ${theme.secondary };
+background-color:transparent;
+  padding: 2px 5px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 30px;
+  width: 150px;
+  font-size: 0.8rem;
+  
+`
+
+
+export const Categories = props => (
+<>
+  {[...props.items, 'All'].map((catagory, i) => <Pill key={i}>{catagory}</Pill>)}
+</>
+);
